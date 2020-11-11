1424b2826ca1055a0690fd66a45bc3eeaa3d0f8f
Administrator <402922965@qq.com>
2020-11-11 13:56:39 +0800

    one tatol commit

---
+++ mm-forum-7005/src/main/java/com/qianfeng/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.qianfeng.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qianfeng.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+}
